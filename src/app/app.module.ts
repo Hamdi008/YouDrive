a7d9c7e6477607cf8ec80305d3ebd59f9f5517cb
Hamdi ELFEKIH <hamdi.elfekih@outlook.com>
2023-12-20 16:01:43 +0100

    Remove default HTML component and create header and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/header/header.component';
+import { FooterComponent } from './Footer/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
